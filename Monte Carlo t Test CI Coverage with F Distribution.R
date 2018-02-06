5be5d3d732a728d5757083b0fedf422c799c1957
Michael Seaman <fourthz@gmail.com>
2018-02-06 15:11:03 -0500

    Initial commit of R Script for F test

---
+++ Monte Carlo t Test CI Coverage with F Distribution.R
@@ -0,0 +1,11 @@
+
+###############################################
+#
+# Monte Carlo Simulation of the t test coverage
+# of the mean when samples are from the F
+# distribution.
+#
+# Michael A. Seaman
+# University of South Carolina
+#
+###############################################
