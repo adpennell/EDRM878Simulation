60628ffc015f602bf50cde74dee204c515a74698
Adam Sokol <asokol@mailbox.sc.edu>
2018-02-06 16:06:11 -0500

    update 1.1

--- Beta distribution simulation.R
+++ Beta distribution simulation.R
@@ -1,12 +1,25 @@
-betaControl <- rbeta(100,1,2,ncp=0)
-mean_beta <- mean(betaControl)
+#Adam Sokol
 #create function 
 
-
-x=2000 #How many times it will run
-for(i in 1:x){
-betaControl <- rbeta(100,1,,ncp=0)
+test_fun <- function(n,alpha,beta,NCP){
+betaControl <- rbeta(n,alpha,beta,ncp=NCP)
+mean_betas <- 1/(1+(beta/alpha))
 beta_T <- t.test(betaControl, alternative = "two.sided", mu=0)
 
-LB<- c(Wilks.Lamb.vec,test1$wilks)
+#beta.out <- vector()
+#beta.out[1] <- beta_T$conf.int[1]
+#beta.out[2] <- beta_T$conf.int[2]
+
+LB_vec<- c(LB_vec,beta_T$conf.int[1])
+UB_vec <- c(UB_vec,beta_T$conf.int[2])
+mean_beta_vec <- c(mean_beta_vec,mean_betas)
+mean_conf<- ifelse(beta_T$conf.int[1]<mean_betas & beta_T$conf.int[2]>mean_betas,1,0)
+
+
+return(dat1)
 }
+
+
+test_fun(100,1,2,0)
+test_dat1 <-replicate(2,test_fun(100,1,2,0))
+test_dat1
