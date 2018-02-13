5492dddebdf2b34ca90c8562f379ec9be03c333b
Michael Seaman <fourthz@gmail.com>
2018-02-13 16:04:26 -0500

    Finished the draft of the F Distribution script.

--- Monte Carlo t Test CI Coverage with F Distribution.R
+++ Monte Carlo t Test CI Coverage with F Distribution.R
@@ -10,6 +10,26 @@
 #
 ###############################################
 
+#I'm setting up some parameters that I can easily change,
+#so I'm putting those right at the top.
+
+#Sample size parameter
+
+sample.size <- 20
+
+#Degrees of freedom for the F distribution
+
+degrees.1 <- 2
+degrees.2 <- 10
+
+#Confidence level
+
+conf.level <- .95
+
+#Number of repetitions
+
+num.rep <- 1000000
+
 #Function for a single replication
 
 MonteF <- function(n, df1, df2, ci.level){
@@ -32,5 +52,10 @@ MonteF <- function(n, df1, df2, ci.level){
   
 }
 
-myresult <- MonteF(20,2,10,.95)
+F.results <- replicate(num.rep,
+                       MonteF(sample.size,
+                              degrees.1,
+                              degrees.2,
+                              conf.level))
 
+prop.coverage <- sum(F.results[3,])/num.rep
