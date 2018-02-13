bf70d77895a11c71e6ba5aa9988ffb95d0020ed8
Ginger Jiang <jiangning0302@gmail.com>
2018-02-13 16:07:52 -0500

    Uniform Simulation

--- Uniform Simulation.R
+++ Uniform Simulation.R
@@ -9,17 +9,19 @@ set.seed(2618)
 #### c is confidence interval, s is the sided test: one sided or two sided test. 
 uniform <- function(x,c,s){
   y <-runif(x)
+  mean.unif = 1/2
   p <- t.test(y, mu=0, alternative=s, conf.level=c)
   ## Dr. Seaman add if else command 
   p <- c("Lower"=p$conf.int[1],"Upper"=p$conf.int[2], 
-  "Rejection"=if ((p$estimate > p$conf.int[1]) & (p$estimate < p$conf.int[2])) 
+  "Rejection"=if ((mean.unif > p$conf.int[1]) & (mean.unif < p$conf.int[2])) 
   result <- 1 else result <- 0)
   return(p)
 }
 
 ### replicate 20 times to draw a sample with sample size 100 from a uniform distribution 
 set.seed(2618)
-mydata <- replicate (20,uniform(100,.95,"two.sided"))
+mydata <- replicate (1000,uniform(100,.95,"two.sided"))
+reject.p <- rowSums(mydata)[3]/1000
 
 ## Save my results as a txt file
 write.table(mydata, "C:/Users/Ginger Jiang/Desktop/mydata.txt", sep="\t")
