8b0c770d43338a91fd9273877d960982003cf8e7
Adam Sokol <asokol@mailbox.sc.edu>
2018-02-20 14:44:49 -0500

    update 1.4

--- Beta distribution simulation.R
+++ Beta distribution simulation.R
@@ -1,10 +1,10 @@
 #Adam Sokol
 #create function 
 
-test_fun <- function(n,alpha,beta,NCP){
+test_fun <- function(n,alpha,beta,NCP,Conf){
 betaControl <- rbeta(n,alpha,beta,ncp=NCP)
 mean_betas <- 1/(1+(beta/alpha))
-beta_T <- t.test(betaControl, alternative = "two.sided", mu=0)
+beta_T <- t.test(betaControl, alternative = "two.sided", mu=0,conf.level = Conf)
 mean_conf<- ifelse(beta_T$conf.int[1]<mean_betas & beta_T$conf.int[2]>mean_betas,1,0)
 
 test_fun.out <- vector()
@@ -18,6 +18,7 @@ return(test_fun.out)
 
 
 
-test_dat1 <-replicate(1000000,test_fun(100,1,2,0))
+test_dat1 <-replicate(9604,test_fun(100,1,2,0,.90))
 
-hit_rate <- sum(test_dat1[4,])/1000000
+hit_rate <- sum(test_dat1[4,])/9604
+hit_rate
