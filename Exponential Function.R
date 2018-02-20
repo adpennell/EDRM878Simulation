4ef0311007610e26abb34efd968f4e112b25f5d5
Bradley Rogers <bdr1@email.sc.edu>
2018-02-20 13:53:30 -0500

    Updated with conf.level and monte carlo package trials

--- Exponential Function.R
+++ Exponential Function.R
@@ -4,18 +4,19 @@
 
 s.size<-30
 Lambda<-1
-n.rep<-100000
+c.level<-.95
+n.rep<-100
 
-x<-replicate(n.rep,exp.sim(s.size,Lambda))
+x<-replicate(n.rep,exp.sim(s.size,Lambda,c.level))
 Percent<-(sum(x[3,])/n.rep)*100
 Percent
 
+?t.test
 
-
-exp.sim<-function(n,x.rate){
+exp.sim<-function(n,x.rate,c.level){
   x.mean<-1/x.rate
   exp.sample<-rexp(n,rate=x.rate)
-  result<-tidy(t.test(exp.sample,mu=x.mean))
+  result<-tidy(t.test(exp.sample,mu=x.mean,conf.level = c.level))
   Estimate<-result$estimate
   L.conf<-result$conf.low
   U.conf<-result$conf.high
@@ -28,7 +29,34 @@ exp.sim<-function(n,x.rate){
 
 
 
+################################################################################################
 
-result<-MonteCarlo(func = exp.sim,nrep=2, param_list = p_list)
+#Sim function for use with  Monte Carlo Package
+
+exp.sim.2<-function(n,x.rate){
+  x.mean<-1/x.rate
+  exp.sample<-rexp(n,rate=x.rate)
+  result<-tidy(t.test(exp.sample,mu=x.mean))
+  Estimate<-result$estimate
+  L.conf<-result$conf.low
+  U.conf<-result$conf.high
+  Decision<-if(x.mean >= result$conf.low & x.mean <=result$conf.high){decision=1}else{decision=0}
+  out=list(L.conf, U.conf, Decision)
+  names(out)<-c("L.conf","U.conf","Decision")
+  out}
+
+exp.sim.2(s.size,Lambda)
+n.monte<-c(30,50,100)
+x.rate.monte<-c(1,2)
+p_list<-list("n"=n.monte, "x.rate"= x.rate.monte)
+result<-MonteCarlo(func = exp.sim.2,nrep=5, param_list = p_list)
 summary(result)
-MakeTable(output=result, rows="n", cols=c("x.rate","x.mean"), digits=2, include_meta=FALSE)
+MakeTable(output=result, rows="n", cols=c("x.rate"), digits=2, include_meta=FALSE)
+
+
+
+
+n = c(2, 3, 5) 
+s = c("aa", "bb", "cc") 
+b = c(TRUE, FALSE, TRUE) 
+df = data.frame(n, s, b) 
